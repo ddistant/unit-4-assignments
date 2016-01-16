38965c7f2dd9b3242d7af3197a11ef5b7b5fe135
Daniel Distant <daniel.distant1@gmail.com>
2016-01-16 09:47:35 -0500

    commit

--- HWfrom1-09-16(SwiftIntro).playground/Contents.swift
+++ HWfrom1-09-16(SwiftIntro).playground/Contents.swift
@@ -12,12 +12,77 @@ https://docs.google.com/document/d/1DQ2aCJ_yUZtazzCfb0PaS81bg61V2ZOSxpABh981xSo/
 
 
 1)
-
-2)
-
-3)
-
-4)
-
-
 */
+
+func findTheMissingNumber(N: Int, list: [Int]) -> Int {
+    list.sort({$0 < $1})
+    var j = 0
+    var missingNum = 0
+    
+    for i in (0..<list.count) {
+        j = i + 1
+        if(j - 1 != 1) {
+            missingNum = i+1
+        }
+    }
+    return missingNum
+}
+
+//2)
+
+func areThereDuplicates(list: [Int]) -> Bool {
+    
+    let sortedList = list.sort({$0 < $1})
+    
+    for i in 0...sortedList.count {
+        if sortedList[i] == sortedList[i + 1] {
+            return true
+        } else if sortedList[i + 1] >= sortedList.count {
+            break
+        }
+    }
+    
+    return false
+}
+
+
+//3)
+
+
+func getSmallestCommmonValue(list1: [Int], list2: [Int]) -> Int? {
+    var combinedList : [Int] = []
+    combinedList.appendContentsOf(list1)
+    combinedList.appendContentsOf(list2)
+    
+    let sortedCombinedList = combinedList.sort({$0 < $1})
+    
+    for i in sortedCombinedList.first!...sortedCombinedList.count {
+        if sortedCombinedList[i] == sortedCombinedList[i + 1] {
+            return sortedCombinedList[i]
+        }
+        
+    }
+    return nil
+}
+
+//4)
+
+func isPalindrome(num: Int) -> Bool {
+    
+    var arr: [Int] = []
+    
+    arr.map(num)
+
+    var reverseArr : [Int] = []
+    
+    for i in arr {
+        if i % 10 == 0 {
+           reverseArr.insert(i, atIndex: 0)
+        }
+    }
+    
+    print(reverseArr)
+    
+    return true
+    
+}
--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -13,17 +13,46 @@ https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/
 
 1)
 
-2)
+Pixel **awesomeFilter(Pixel image[][], int width, int height) {
+for (int i = 0; i < width; i++) {
+for (int j = 0; j < height; j++) {
+[image[i][j] makeMoreAwesome];
+}
+}
+return image;
+}
+
+a) memory access: [image[i][j] makeMoreAwesome]
+
+b) O(^2)
+
+c) O(n^4): the loop is running (n^2) times, so that times (n^2) is (n^4)
+
+2) 
+
+a) O(n^3), O(n^4), O(n^2) : O(n^4) because its the slowest (biggest) one
+
+b) O(n)
+
+c) O(n log n)
+
+d) O(log n) because its a binary search of a sorted array
 
 3)
 
-4)
+a) tree
 
-5)
+b) set or an array
 
-6)
+c) tree
 
-7)
+4) O(n)
+
+5) O(m)
+
+6) O(log n): any time you're constantly dividing by two (like in binary search), its a pretty good chance its O(log n)
+
+7) 
 
 
 */
